f703fbc460fd78f50372244ed783e22bb8a63231
Bo Yang <yangb05@pcl.ac.cn>
2022-09-07 16:27:50 +0800

    add run.py

---
+++ run.py
@@ -0,0 +1,49 @@
+import argparse
+import importlib
+from pathlib import Path
+
+import mindspore
+from mindspore import context
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # training params
+    parser.add_argument('--algo', default='SenticBERT_ABSA', type=str, help='the algorithm need to train or eval.')
+    parser.add_argument('--data_dir', default=Path('data/SenticBERT_ABSA_data'), type=Path)
+    parser.add_argument('--dataset', default='rest16', type=str, help='twitter, restaurant, laptop')
+    parser.add_argument('--ckpt', default=Path('checkpoints/ms_intergcn_bert_rest16_acc_0.9075_f1_0.7239.ckpt'),
+                        type=str, help='The checkpoint used in eval mode.')
+    parser.add_argument('--valset_ratio', default=0.1, type=float)
+    parser.add_argument('--bert_tokenizer', default='bert-base-chinese', type=str, help='The tokenizer used in ABSA.')
+    parser.add_argument('--batch_size', default=16, type=int, help='try 16, 32, 64 for BERT models')
+    parser.add_argument('--num_workers', default=1, type=int)
+    parser.add_argument('--max_seq_len', default=85, type=int)
+    parser.add_argument('--num_epochs', default=20, type=int, help='try larger number for non-BERT models')
+    parser.add_argument('--warmup', default=5e-2, type=float)
+    parser.add_argument('--log_step', default=10, type=int)
+    parser.add_argument('--patience', default=5, type=int)
+    parser.add_argument('--device', default='GPU', type=str, choices=['Ascend', 'CPU', 'GPU'])
+    parser.add_argument('--seed', default=1000, type=int, help='set seed for reproducibility')
+    # optimizer params
+    parser.add_argument('--lr', default=5e-5, type=float, help='try 5e-5, 2e-5 for BERT, 1e-3 for others')
+    parser.add_argument('--weight_decay', default=0.00001, type=float)
+    # train/eval
+    parser.add_argument('--mode', default='train', type=str, choices=['train', 'eval'])
+    parser.add_argument('--graph_mode', default=True, type=bool)
+
+    opt = parser.parse_args()
+    mindspore.set_seed(opt.seed)
+    if opt.graph_mode:
+        context.set_context(mode=context.GRAPH_MODE, device_target=opt.device)
+    else:
+        context.set_context(mode=context.PYNATIVE_MODE, device_target=opt.device)
+    algo = importlib.import_module(opt.algo)
+    print(f'Start running algorithm {opt.algo} in {opt.mode} mode.')
+    if opt.mode == 'train':
+        ins = algo.Instructor(opt)
+        ins.train()
+    else:
+        ins = algo.Instructor(opt)
+        ins.eval(str(opt.data_dir / opt.ckpt))
+
